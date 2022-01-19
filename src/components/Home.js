7e141ee36100f7cfababb03337d07f726a1b88c1
Alexander <al.x.falconer@gmail.com>
2022-01-19 01:33:48 -0500

    cleaning up

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,14 @@ const Home = () => (
         <a href="/gallery">
         <h3><img className="twomblyhome" src={twomblyhome} alt="twomblyhome"/></h3>
         </a>
-        <div id="viewerContainer">
-        <p>Abstract painter Cy Twombly (1928-2011) drew on poetry to inspire his chaotic works, gathering lines from disparate sources to provide foundations for his abstractions.</p>
-        <p>Here on POEMS FOR CY we ask users to do the opposite; to create poetry by drawing inspiration from artworks themselves.</p>
-        <p>Users can build on eachother's lines, creating a confluence of varying perspectives and styles.</p>
-        <p>The artworks have been pulled from the Art Institute of Chicago and presented without context in order to ensure complete creative freedom for our poets.</p>
-        <p>Click the image above to enter the gallery and get started.</p>
-        </div>
+       
+            <br></br>
+        <p id="viewerContainer"><br></br>Abstract painter Cy Twombly (1928-2011) drew on poetry to inspire his chaotic works, gathering lines from disparate sources to provide foundations for his abstractions.
+        Here on POEMS FOR CY we ask users to do the opposite; to create poetry by drawing inspiration from artworks themselves.
+        Users can build on eachother's lines, creating a confluence of varying perspectives and styles.
+        The artworks have been pulled from the Art Institute of Chicago and presented without context in order to ensure complete creative freedom for our poets.
+        Click the image above to enter the gallery and get started.</p>
+        
     </div>
   ); 
 
